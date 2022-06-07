8709c1dc38350080868953d2a2556c6f023644cb
Lyn Finman <lynfinman@gmail.com>
2022-06-07 16:00:55 -0400

    commit tree 00ed8576a66ad34bb871ad7f0d86f321136a9aa3

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+
+const Bio = (props) => {
+  if (props.bio) {
+    return <p>{props.bio}</p>
+  } else {
+    return null;
+  }
+}
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <Bio bio={props.bio} />
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
